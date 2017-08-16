e6ad3c20cb28af2506665f336b44aa93012505d1
Donovan Muller <donovan.muller@gmail.com>
2017-08-16 22:55:29 +0200

    initial commit by Atomist

---
+++ src/main/java/io/switchbit/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package io.switchbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+        System.out.println("This is just a test...");
+    }
+}
